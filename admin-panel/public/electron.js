b5b23a6b9b366fa45aa28d59587646afba084517
Ry Torilla <ray.torilla187@gmail.com>
2025-11-28 23:24:30 +0800

    chore(electron): include electron and preload in public so CRA build copies them; add clean script

---
+++ admin-panel/public/electron.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  const startUrl = process.env.ELECTRON_START_URL;
+  if (startUrl) {
+    win.loadURL(startUrl);
+  } else {
+    win.loadFile(path.join(__dirname, 'index.html'));
+  }
+
+  win.setMenu(null);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit();
+});
